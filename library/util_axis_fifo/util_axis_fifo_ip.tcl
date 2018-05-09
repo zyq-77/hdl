50c906d1c00c63cf117373becbd47f923a55700c
Istvan Csomortani <istvan.csomortani@analog.com>
2018-05-09 10:06:23 +0300

    util_axis_fifo: Support for asymmetric aspect ratio
    Note: because verilog has a limited support of dynamic port widths, explicitly
    define the read and write address width using parameters. The FIFO depth will
    be defined by the write address width, the correctness of the read address
    width will be checked by the packaging script.
    
    The following equation should be respected:
    (2^WR_ADDRESS_WIDTH)*WR_DATA_WIDTH == (2^RD_ADDRESS_WIDTH)*RD_DATA_WIDTH

--- library/util_axis_fifo/util_axis_fifo_ip.tcl
+++ library/util_axis_fifo/util_axis_fifo_ip.tcl
@@ -4,10 +4,9 @@ source $ad_hdl_dir/library/scripts/adi_ip_xilinx.tcl
 
 adi_ip_create util_axis_fifo
 adi_ip_files util_axis_fifo [list \
-	"address_gray.v" \
-	"address_gray_pipelined.v" \
-	"address_sync.v" \
+	"address_generator.v" \
 	"../common/ad_mem.v" \
+	"../common/ad_mem_asym.v" \
 	"util_axis_fifo.v" \
 ]
 
@@ -37,5 +36,16 @@ adi_add_bus "m_axis" "master" \
 
 adi_add_bus_clock "m_axis_aclk" "m_axis" "m_axis_aresetn"
 adi_add_bus_clock "s_axis_aclk" "s_axis" "m_axis_aresetn"
+
+## TODO: Validate RD_ADDRESS_WIDTH
+
+## add parameters to GUI
+
+ipgui::add_param -name {WR_DATA_WIDTH} -component [ipx::current_core] -display_name {Write Data Width} -show_label {true} -show_range {true} -widget {}
+ipgui::add_param -name {RD_DATA_WIDTH} -component [ipx::current_core] -display_name {Read Data Width} -show_label {true} -show_range {true} -widget {}
+ipgui::add_param -name {WR_ADDRESS_WIDTH} -component [ipx::current_core] -display_name {Address width of the write side. The actual depth of the FIFO} -show_label {true} -show_range {true} -widget {}
+ipgui::add_param -name {RD_ADDRESS_WIDTH} -component [ipx::current_core] -display_name {Address width of the read side.} -show_label {true} -show_range {true} -widget {}
+ipgui::add_param -name {ASYNC_CLK} -component [ipx::current_core] -display_name {Asynchronous write/read interface} -show_label {true} -show_range {true} -widget {}
+ipgui::add_param -name {S_AXIS_REGISTERED} -component [ipx::current_core] -display_name {Registered data output on AXIS slave} -show_label {true} -show_range {true} -widget {}
 
 ipx::save_core [ipx::current_core]
