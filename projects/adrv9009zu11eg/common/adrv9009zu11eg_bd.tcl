38c4fe849c40fc88d11729467e6960f93f58e06c
Adrian Costina <adrian.costina@analog.com>
2019-07-02 09:37:03 +0100

    adrv9009_zu11eg: Update FPGA to -2

--- projects/adrv9009zu11eg/common/adrv9009zu11eg_bd.tcl
+++ projects/adrv9009zu11eg/common/adrv9009zu11eg_bd.tcl
@@ -188,12 +188,13 @@ create_bd_intf_port -mode Slave -vlnv xilinx.com:interface:diff_clock_rtl:1.0 dd
 
 ad_ip_instance ip:ddr4 ddr4_1
 ad_ip_parameter ddr4_1 CONFIG.C0.DDR4_DataWidth {32}
+ad_ip_parameter ddr4_1 CONFIG.C0.DDR4_TimePeriod {750}
 ad_ip_parameter ddr4_1 CONFIG.C0.DDR4_AxiDataWidth {256}
 ad_ip_parameter ddr4_1 CONFIG.C0.DDR4_AxiAddressWidth {31}
 ad_ip_parameter ddr4_1 CONFIG.C0.DDR4_InputClockPeriod {3334}
 ad_ip_parameter ddr4_1 CONFIG.C0.DDR4_MemoryPart {MT40A512M16HA-075E}
 ad_ip_parameter ddr4_1 CONFIG.C0.BANK_GROUP_WIDTH {1}
-ad_ip_parameter ddr4_1 CONFIG.C0.DDR4_CasLatency {18}
+ad_ip_parameter ddr4_1 CONFIG.C0.DDR4_CasLatency {19}
 
 ad_connect ddr4_rtl_1 ddr4_1/C0_DDR4
 
